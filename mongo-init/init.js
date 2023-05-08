7d45b63f7a7fb8d8f08757a55288834a7497d07b
b0006 <qsefthuken@gmail.com>
2023-05-08 23:12:39 +0700

    bugfix: docker compose up

--- mongo-init/init.js
+++ mongo-init/init.js
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { MongoClient } = require('mongodb');
-const uri = 'mongodb://mongodb:27017/';
+const uri = 'mongodb://container_mongodb:27017/';
 const client = new MongoClient(uri);
 
-client.connect((err) => {
+client.connect(async (err) => {
   if (!err) {
     console.log('connection created');
   }
   const newDB = client.db('chess');
-  newDB.createCollection('test2'); // This line is important. Unless you create collection you can not see your database in mongodb .
+  newDB.createCollection('test-collection'); // This line is important. Unless you create collection you can not see your database in mongodb .
   client.close();
 });
