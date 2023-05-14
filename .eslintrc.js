0f9cdf6ecdda4700ce251bb472a956b9d841b364
Kasikov <akasikov@vtb.ru>
2023-05-14 22:28:18 +0700

    refactor: linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,19 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-duplicate-imports': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        tabWidth: 2,
+        printWidth: 100,
+        singleQuote: true,
+        trailingComma: 'all',
+        jsxSingleQuote: true,
+        bracketSpacing: true,
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
